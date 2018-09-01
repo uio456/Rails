06eda17b5e2df88e2daeaeaf1f9e408994b7d706
Duncan <loxa10077@gmail.com>
2018-09-01 16:36:20 +0800

    can create candidate on index page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,6 +2,24 @@ class CandidatesController < ApplicationController
 
   def index
     @candidates = Candidate.all
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(params_candidate)
+
+    if @candidate.save
+      flash[:notice] = "建立成功"
+    else
+      flash[:alert] = "建立有誤"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def params_candidate
+    params.require(:candidate).permit(:name, :age, :politics, :party, :votes)
   end
 
 end
