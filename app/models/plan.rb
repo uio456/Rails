4ffc28c9884108014b16cfbb4b7f55291ade9ed9
Duncan <loxa10077@gmail.com>
2018-09-13 13:46:04 +0800

    add comment for post and plan using polymorphic associations

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,8 @@
 class Plan < ApplicationRecord
   validates_presence_of :title
   belongs_to :member
+  has_many :comments, :as => :commentable
+
   attr_accessor :tag
   acts_as_taggable_on :tags
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates_presence_of :title, :content
   belongs_to :member
+  has_many :comments, :as => :commentable
+
   # attr_accessor :tag_list
   acts_as_taggable_on :tags
 end
